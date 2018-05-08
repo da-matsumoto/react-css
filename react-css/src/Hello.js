4ee2bdbe4a97f3a3a1570c30e13a09651d886d87
da-matsumoto <da.mats1990@gmail.com>
2018-05-08 22:17:51 +0900

    css in js

---
+++ react-css/src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const css = {
+    color: 'red',
+    fontSize: '240px'
+}
+
+const Hello = () => {
+    return <div style={css}>Hello, world</div>;
+}
+
+export default Hello
